e37c463f6c1dead339efff55114f6ea69cbb7302
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-04-27 23:04:23 -0700

    fix logic and data parsing

--- scripts/TriviaApi.js
+++ scripts/TriviaApi.js
@@ -1,65 +1,16 @@
-const TRIVIA_APP_BASE_URL = "https://opentdb.com/api.php?";
-const QUESTION_TYPE = "multiple"; // multiple choice by default
-const DIFFICULTIES = ["easy", "medium", "hard"];
-const DIFFICULTY = DIFFICULTIES[0]; // easy
-const ENCODING = ""; // put nothing for default encoding
+const TRIVIA_APP_BASE_URL =
+  "https://opentdb.com/api.php?amount=10&category=18&type=multiple";
 export class TriviaApi {
   constructor() {
     this.baseUrl = TRIVIA_APP_BASE_URL;
   }
 
-  getQuery(numberOfQuestions, category, difficulty, questionType) {
-    let query = this.baseUrl;
-
-    if (typeof numberOfQuestions === "number" && numberOfQuestions > 0) {
-      query += `amount=${this.numberOfQuestions}`;
-    }
-
-    if (typeof category === "number") {
-      // unsure of all category numbers, another check should be added here
-      query += `&category=${category}`;
-    }
-
-    if (this.isValidDifficulty(difficulty)) {
-      query += `&difficulty=${difficulty}`;
-    }
-
-    if (typeof questionType === "string" && questionType === QUESTION_TYPE) {
-      query += `&type=${QUESTION_TYPE}`;
-    }
-
-    return query;
-  }
-
-  isValidDifficulty(difficulty) {
-    return (
-      typeof difficulty === "string" &&
-      (difficulty === DIFFICULTIES[0] ||
-        difficulty === DIFFICULTIES[1] ||
-        difficulty === DIFFICULTIES[2])
-    );
-  }
-
   async getTrivia() {
     try {
       const response = await axios.get(
-        `${this.baseUrl}amount=10` //simple query with 10 questions
+        `${this.baseUrl}` //simple query with 10 computer science questions
       );
-      console.log(response.data); // should return array of question objects
-      return response.data;
-    } catch (error) {
-      console.log(`Catching error from api call`, error);
-    }
-  }
-
-  // another version of getTrivia with more options, for getting a single question only
-  async getTriviaQuestion(category, difficulty, questionType) {
-    try {
-      const response = await axios.get(
-        this.getQuery(10, category, difficulty, questionType)
-      );
-      console.log("response.data in getTriviaQuestion:", response.data); // should return array of question
-      return response.data;
+      return response.data.results;
     } catch (error) {
       console.log(`Catching error from api call`, error);
     }
@@ -67,10 +18,3 @@ export class TriviaApi {
 }
 
 export default TriviaApi;
-export {
-  TRIVIA_APP_BASE_URL,
-  QUESTION_TYPE,
-  DIFFICULTIES,
-  DIFFICULTY,
-  ENCODING,
-};
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,28 +1,11 @@
 import TriviaApi from "./TriviaApi.js";
 
-/* -------------------------------------------------------------------------- */
-/*                               sample repsonse                              */
-/* -------------------------------------------------------------------------- */
-// "results": [
-//     {
-//       "type": "multiple",
-//       "difficulty": "medium",
-//       "category": "Science: Computers",
-//       "question": "Which of these is the name for the failed key escrow device introduced by the National Security Agency in 1993?",
-//       "correct_answer": "Clipper Chip",
-//       "incorrect_answers": [
-//         "Enigma Machine",
-//         "Skipjack",
-//         "Nautilus"
-//       ]
-//     },
-// ]
-
 const triviaApi = new TriviaApi();
-const questionListEl = document.querySelector(".questions-list");
+const questionListEl = document.querySelector(".question-list");
 const answersListEl = document.querySelectorAll(".inputs__btn");
 const balanceDisplayEl = document.querySelector(".player-balance");
-let count = 0;
+let questions = []; // store question from api
+let currentQuestion = 0; // keep track of the question displayed
 let balance = 0;
 
 // Create and set element function
@@ -38,79 +21,79 @@ function updateAndDisplayBalance() {
 }
 
 // display function
-async function displayQuestion() {
+async function displayQuestionAndAnswer() {
   try {
-    // for each question object
-    // fetch a single question
-    const questions = await triviaApi.getTriviaQuestion(9, "easy", "multiple"); // it gets 50 questions if i just request 1???
-    console.log("Questions in displayQuestion:", questions);
-    // Select the first 10 questions
-    const selectedQuestions = questions.slice(0, 10);
-    console.log("10 Questions only", selectedQuestions);
-    const singleQuestion = questions.results[0];
-    console.log("Question in displayQuestion:", singleQuestion);
+    // check if the current question is less than the array
+    if (currentQuestion < questions.length) {
+      const singleQuestion = questions[currentQuestion]; // get a single question
 
-    // display the question
-    const questionCardEl = createElement("li", "question__card"); // create card element to contain (question)
-    const questionEl = createElement("p", "question__card-content"); // create li element -> for question
-    questionEl.innerText = singleQuestion.question; // set inner li to question property of object
-    questionCardEl.append(questionEl);
-    questionListEl.append(questionCardEl); // append card with question to list
-
-    displayAnswers(
-      singleQuestion.incorrect_answers,
-      singleQuestion.correct_answer
-    );
+      // display the question
+      const questionCardEl = createElement("li", "question__card"); // create card element to contain (question)
+      const questionEl = createElement("p", "question__card-content"); // create li element -> for question
+      questionEl.innerText = decodeURIComponent(singleQuestion.question); // set inner li to question property of object
+      questionCardEl.append(questionEl); // append trivia question to card element
+      questionListEl.append(questionCardEl); // append card with question to list
+      displayAnswers(singleQuestion); // display answers from question
+    } else {
+      console.log("You have lost");
+    }
   } catch (error) {
-    console.log(error);
+    console.log("Could not fetch questions", error);
   }
 }
 
 // display answers
-function displayAnswers(incorrect_answers, correctAnswer) {
-  const allAnswers = [...incorrect_answers, correctAnswer];
-  try {
-    console.log("All answers from display answers");
-    const answerButtons = document.querySelectorAll(".inputs__btn");
-    console.log("Answer buttons:", answerButtons); // Debug statement
-    allAnswers.forEach((answer, index) => {
-      answersListEl[index].innerText = answer;
-      //   const answerEl = createElement("button", "input__btn");
-      //   answerEl.innerText = answer;
-      //   answersListEl.appendChild(answerEl);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+function displayAnswers(question) {
+  answersListEl.innerHTML = "";
+  const allAnswers = [...question.incorrect_answers, question.correct_answer];
+  allAnswers.forEach((answer, index) => {
+    answersListEl[index].innerText = decodeURIComponent(answer);
+  });
 }
 
 // function for users answers
 function answerLogic(selectedAnswer, correctAnswer) {
   if (selectedAnswer === correctAnswer) {
     balance += 100000;
+    updateAndDisplayBalance();
+    updateProgressBar();
+    questionListEl.innerHTML = ""; // clear previous question
+    answersListEl.innerHTML = ""; // clear previous answers
+    currentQuestion++;
+    console.log("Current Question:", currentQuestion);
+    displayQuestionAndAnswer(); // get the question and display next one
   } else {
-    balance += 100000;
+    console.log("Incorrect Answer!");
+    balance -= 100000; // decrease the balance
   }
-  count++; // next question
-  updateAndDisplayBalance(); // update balance
-  updateProgressBar(); // update progress bar
-  questionListEl.innerHTML = ""; // Clear previous question
-  answersListEl.innerHTML = ""; // Clear previous answers
-  displayQuestion(); // Display next question
 }
 
 // submit button
 const selectButton = () => {
-  console.log("inside select button list of buttons", answersListEl);
   answersListEl.forEach((answer) => {
-    console.log("current answer", answer);
+    // grab all buttons and add listener
     answer.addEventListener("click", async (event) => {
       try {
         event.preventDefault();
-        alert("Button works");
-        const selectedAnswer = answer.innerText;
+        const selectedAnswer = event.target.innerText; // selected button
+        const correctAnswer = questions[currentQuestion].correct_answer; // get answer from array
+        const formatSelectedAnswer = selectedAnswer.trim().toLowerCase(); // trim whitespace and lowercase to match for selected answer
+        const formatCorrectAnswer = correctAnswer.trim().toLowerCase(); // trim whitespace and lowercase to match for correct answer
+        console.log(
+          `seeing if they equal to: ${
+            formatCorrectAnswer === formatSelectedAnswer
+          }`
+        );
+        console.log(
+          "format selected answer in event listener:",
+          formatSelectedAnswer
+        );
+        console.log(
+          "format correct answer in event listener:",
+          formatCorrectAnswer
+        );
 
-        answerLogic(selectedAnswer);
+        answerLogic(formatSelectedAnswer, formatCorrectAnswer); // check if they are equal
       } catch (error) {
         console.log("Could not display next question", error);
       }
@@ -118,27 +101,22 @@ const selectButton = () => {
   });
 };
 
+// update Progress Bar
 function updateProgressBar() {
   let progressBar = document.querySelector(".progress_bar__progress");
-  progressBar.style.width = count * 10 + "%";
+  progressBar.style.width = currentQuestion * 10 + "%";
 }
 
 // main point of execution
-function main() {
+async function main() {
   console.log("Running main()...");
-  // step 0 - default loadout?
-  // step 1 - display start page?, wait for user to press start
-  // step 2 - get question
-  // step 3 - display question
-  displayQuestion();
-  selectButton(); //set up event listeners
-  updateProgressBar();
-
-  // step 4 - get answer from user
-
-  // step 5 - update settings/display
-  // step 6 - loop back to step 3 until no questions left
-  // step 7 - display game results
+  try {
+    questions = await triviaApi.getTrivia(); // fetch 10 questions from api and store in array
+    displayQuestionAndAnswer(); // display one question
+    selectButton(); // on click checks answers
+  } catch (error) {
+    console.log("Could not fetch the questions", error);
+  }
 }
 
-main();
+main(); // call main
